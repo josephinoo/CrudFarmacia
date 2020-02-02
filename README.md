ccf8874920a1ab17250452d3dc9e571757cb1ea1
Joseph Danilo Avila Alvarez <josdavil@espol.edu.ec>
2020-02-02 07:33:46 -0500

    New Project

---
+++ README.md
@@ -0,0 +1,13 @@
+# JAVAFX LOGIN UI DESIGN WITH MYSQL DB INTERGRATION
+
+Focus areas
+
+- UI Design
+- DB integration
+- Insert - Retrieve
+
+A simple sample to get you started.
+
+![](https://github.com/k33ptoo/JavaFX-MySQL-Login/blob/master/img.png)
+
+![](https://github.com/k33ptoo/JavaFX-MySQL-Login/blob/master/img2.png)
---
+++ src/utils/ConnectionUtil.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author oXCToo
+ */
+public class ConnectionUtil {
+    Connection conn = null;
+    public static Connection conDB()
+    {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Javimarc", "root", "rootroot");
+            System.out.println("Estoy adentro");
+            return con;
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.err.println("ConnectionUtil : "+ex.getMessage());
+            System.out.println("  No se pudo conectar a la bases de Datos ");
+            ex.getStackTrace();
+           return null;
+        }
+    }
+    //make sure you add the lib
+}
